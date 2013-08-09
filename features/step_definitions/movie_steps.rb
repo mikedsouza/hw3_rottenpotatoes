eaa85c642ddda299fc5e27332a6bf28ca5a7673d
Michael DSouza <mikejdsouza@gmail.com>
2013-08-09 14:11:32 -0700

    HW Assignment3 Parts 1-3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -15,9 +15,10 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  e1.should <= e2
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -61,4 +62,34 @@ Then /^I should see all of the movies$/ do
   @num_movies_in_db = Movie.all.length
   @num_movies_on_page = page.body.split("<tr>").length - 2 # need to remove the head <tr> tag and subtract 1 for the extra split
   assert_equal @num_movies_in_db, @num_movies_on_page
+end
+
+Then /^I should see the movies sorted alphabetically$/ do
+  @movie_table_rows = page.body.split("<tr>")[2..-1]       #returns an array of table rows (without the leading <tr> tag)
+  @ordered_movie_titles = @movie_table_rows.map do |row|
+    row[/[^>]*>([^<]*)/, 1]                              #isolates the movie title (by looking between the <td> and </td> tags)
+  end
+  @ordered_movie_titles.each do |movie|
+    if @previous_movie
+      step %Q(Then I should see "#{@previous_movie}" before "#{movie}")
+      @previous_movie = movie
+    else
+      @previous_movie = movie
+    end
+  end
+end
+
+Then /^I should see the movies sorted by release date$/ do
+  @movie_table_rows = page.body.split("<tr>")[2..-1]       #returns an array of table rows (without the leading <tr> tag)
+  @ordered_release_dates = @movie_table_rows.map do |row|
+    row[/(.*\n.*){2}\n[^>]*>([^<]*)/, 2]                   #isolates the release_date (strips first two lines, then looks for
+  end                                                      #the content between the <td> and </td> tags)
+  @ordered_release_dates.each do |date|
+    if @previous_date
+      step %Q(Then I should see "#{@previous_date}" before "#{date}")
+      @previous_date = date
+    else
+      @previous_date = date
+    end
+  end
 end
